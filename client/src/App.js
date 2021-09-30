d293eca8cae727c1c160fe47f7f85f94de4c2eff
Victor Moscone <victormoscone@gmail.com>
2021-09-30 11:49:46 -0700

    Feat: Site now pulls and sets a state of all character data provided by the API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [characterData, setCharacterData] = useState('')
+
+  useEffect(() => {
+    if (characterData === '') {
+      (async () => {
+        try {
+          const { data } = await axios.get(
+            `https://akabab.github.io/superhero-api/api/all.json`,
+          );
+          setCharacterData(data);
+        } catch (err) {
+          console.log(err);
+        };
+      })();
+    };
+    console.log(characterData);
+  });
+
   return (
     <div className="App">
     </div>
