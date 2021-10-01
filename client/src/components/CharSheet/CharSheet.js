56a15929232cfa5304c8fbf64171386079dc60ec
Victor Moscone <victormoscone@gmail.com>
2021-10-01 01:43:20 -0700

    Feat: Added a calculator to determine the total stat values of a character for display.

--- client/src/components/CharSheet/CharSheet.js
+++ client/src/components/CharSheet/CharSheet.js
@@ -2,6 +2,17 @@ import React from 'react'
 import './CharSheet.css';
 
 const CharSheet = (props) => {
+
+    let statSquish = props.chosenCharacter.powerstats;
+
+    let totalStats = 0;
+
+    let statCalc = () => {
+        totalStats = statSquish.intelligence + statSquish.strength + statSquish.speed + statSquish.durability + statSquish.power + statSquish.combat;
+    };
+
+    statCalc();
+
     return (
         <div className="grid-container">
             <div className="item1">{props.chosenCharacter.name}</div>
@@ -12,12 +23,13 @@ const CharSheet = (props) => {
             <div className="item6">Durability:</div>
             <div className="item7">Power:</div>
             <div className="item8">Combat:</div>
-            <div className="item9">{props.chosenCharacter.powerstats.intelligence}</div>
-            <div className="item10">{props.chosenCharacter.powerstats.strength}</div>
-            <div className="item11">{props.chosenCharacter.powerstats.speed}</div>
-            <div className="item12">{props.chosenCharacter.powerstats.durability}</div>
-            <div className="item13">{props.chosenCharacter.powerstats.power}</div>
-            <div className="item14">{props.chosenCharacter.powerstats.combat}</div>
+            <div className="item9">{statSquish.intelligence}</div>
+            <div className="item10">{statSquish.strength}</div>
+            <div className="item11">{statSquish.speed}</div>
+            <div className="item12">{statSquish.durability}</div>
+            <div className="item13">{statSquish.power}</div>
+            <div className="item14">{statSquish.combat}</div>
+            <div className="item15">Total Stats: {totalStats}</div>
         </div>
     )
 };
