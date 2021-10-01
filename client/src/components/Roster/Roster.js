cc256a6ed36a11be6f86fe41dc8be3ca4e76b8fc
Victor Moscone <victormoscone@gmail.com>
2021-09-30 23:47:00 -0700

    Feat: When selecting a team, a roster of names now appears.

--- client/src/components/Roster/Roster.js
+++ client/src/components/Roster/Roster.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Roster.css';
 
 const Roster = (props) => {
 
-    
+    const [teamMap, setTeamMap] = useState();
+
+    useEffect(() => {
+        if (props.chosenTeam !== 'Not Selected') {
+            setTeamMap(props.chosenTeam.map( 
+                (character) =>
+                  {
+                    return <p key={character.id}>{character.name}</p>;
+                  }
+                )); 
+        }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.chosenTeam]);
 
     return (
-        <div>
+        <div className='Roster'>
+            {teamMap}
         </div>
     );
 }
